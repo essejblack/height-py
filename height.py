8a287d26d17e817f14492f65f12bc024d6002242
Ehsan Jahanara <ehsan.jnra@gmail.com>
2025-03-14 16:24:18 +0330

    init

---
+++ height.py
@@ -0,0 +1,8 @@
+from tkinter import Tk,Button,Label,DoubleVar,Entry
+
+window = Tk()
+window.title("Feet To Meter Conversion App")
+window.configure(background="#d9d9d9")
+window.geometry("300x300")
+window.resizable(False,False)
+window.mainloop()
